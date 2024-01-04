a0eebdfa64170ffd3f5cc06469eeaad069e51c30
stwardak <148924249+stwardak@users.noreply.github.com>
2024-01-04 12:53:33 -0600

    added routes for rooms, reservations, and users to config/routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+get "/users" => "users#index"
+get "/users/:id" => "users#show"
+post "/users" => "users#create"
+patch "/users/:id" => "users#update"
+delete "/users/id" => "users#destroy"
+
+get "/rooms" => "rooms#index"
+get "/rooms/:id" => "rooms#show"
+post "/rooms" => "rooms#create"
+patch "/rooms/:id" => "rooms#update"
+delete "/rooms/id" => "rooms#destroy"
+
+get "/reservations" => "reservations#index"
+get "/reservations/:id" => "reservations#show"
+post "/reservations" => "reservations#create"
+patch "/reservations/:id" => "reservations#update"
+delete "/reservations/id" => "reservations#destroy"
+
+
 end
