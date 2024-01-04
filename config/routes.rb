06459663f63eca0092df1275152327b40b853f34
Joshua Yee <joshyee16@gmail.com>
2024-01-04 12:12:45 -0800

    add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   patch "/users/:id" => "users#update"
   delete "/users/id" => "users#destroy"
 
+  post "/sessions" => "sessions#create"
+
   get "/rooms" => "rooms#index"
   get "/rooms/:id" => "rooms#show"
   post "/rooms" => "rooms#create"
